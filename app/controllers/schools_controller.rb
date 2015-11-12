22a73958b73d3956911b9192017c5197023ecd25
Nguyen Thi Phuong Thuy <p-thuy@oa.netmile.lan>
2015-11-12 19:18:53 +0900

    update

--- app/controllers/schools_controller.rb
+++ app/controllers/schools_controller.rb
@@ -2,4 +2,20 @@ class SchoolsController < ApplicationController
   def index
     @schools = School.all
   end
+
+  def new
+    @school = School.new
+  end
+
+  def create
+    @school = School.new(school_params)
+    Rails.logger.debug(@school.to_json)
+    @school.save
+    redirect_to schools_path
+  end
+
+  private
+    def school_params
+      params.require(:school).permit(:name, :int)
+    end
 end
